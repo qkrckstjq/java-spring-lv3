21d70649a362c61b9d33478bb28da1d5d771f7eb
qkrckstjq <cksk49@naver.com>
2024-03-08 19:38:44 +0900

    롤백

--- src/main/java/com/example/backoffice/model/entity/Member.java
+++ src/main/java/com/example/backoffice/model/entity/Member.java
@@ -22,21 +22,21 @@ public class Member {
     @Column(name = "password")
     private String password;
 
-    @Column(name = "gender")
-    private String gender;
+    @Column(name = "part")
+    private String part;
 
     @Column(name = "auth")
     private String auth;
     public Member(SignupRequestDto requestDto) {
         this.email = requestDto.getEmail();
         this.password = requestDto.getPassword();
-        this.gender = requestDto.getGender();
+        this.part = requestDto.getPart().toString();
         this.auth = requestDto.getAuth().toString();
     }
     public Member(SignupRequestDto requestDto, String password) {
         this.email = requestDto.getEmail();
         this.password = password;
+        this.part = requestDto.getPart().toString();
         this.auth = requestDto.getAuth().toString();
-        this.gender = requestDto.getGender();
     }
 }
